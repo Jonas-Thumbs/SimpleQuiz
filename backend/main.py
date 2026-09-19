35282e187d59c3b6c12c713c3b938e6ec4dfd727
naugoose <ga496@nau.edu>
2026-09-19 03:04:27 -0700

    Added SQLModel database

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
 #added httpeexception for possible eror messages.
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
+from sqlmodel import Session, select
+from backend.database import create_db_and_tables, get_session
+from backend.models import Student
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 #The simplest possible backend. From: https://fastapi.tiangolo.com/tutorial/first-steps/
 @app.get("/")
