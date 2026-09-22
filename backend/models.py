abec46a0610983eb4129b309203a6ac5cc3e0c15
naugoose <ga496@nau.edu>
2026-09-21 17:37:02 -0700

    Finished database. Will adjusts conflicts with most current version.

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
 
-class User(SQLModel, table=True):
+class Student(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
-    email: setattr
+    email: str
