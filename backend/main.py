6645862c8e74438d3ee030543333c875a68eb639
Gustavo Avina <125099133+naugoose@users.noreply.github.com>
2026-09-21 17:52:39 -0700

    Revert "D1 branch"

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,8 @@
 #added httpeexception for possible eror messages.
-from contextlib import asynccontextmanager
-from fastapi import FastAPI, HTTPException, Depends
-from sqlmodel import Session, select
-from backend.database import create_db_and_tables, get_session
-from backend.models import Student
+from fastapi import FastAPI, HTTPException
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    create_db_and_tables()
-    yield
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 #The simplest possible backend. From: https://fastapi.tiangolo.com/tutorial/first-steps/
 @app.get("/")
@@ -24,15 +16,3 @@ async def test_put(message: str):
         "status": "success",
         "yourmessage": message
     }
-
-@app.post("/students/", response_model=Student)
-def create_student(student: Student, session: Session = Depends(get_session)):
-    session.add(student)
-    session.commit()
-    session.refresh(student)
-    return student
-
-@app.get("/students/", response_model=list[Student])
-def read_students(session: Session = Depends(get_session)):
-    students = session.exec(select(Student)).all()
-    return students
