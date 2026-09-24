71401a34ed6e46f9b5e89fed2cb12637684182af
Cody Hess <107787139+CodyHesss@users.noreply.github.com>
2026-09-23 21:55:39 -0700

    Added doc strings BackendMessage.jsx
    Added doc strings

--- frontend/src/components/BackendMessage.jsx
+++ frontend/src/components/BackendMessage.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react'
-
+/**
+ * Retrieves a message from the backend when the component loads
+ * and displays the returned message.
+ */
 function BackendMessage() {
   const [message, setMessage] = useState('')
 
